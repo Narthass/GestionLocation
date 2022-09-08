c1a119652263d2ce2192b148f2f951fb4ab3f6d8
Jordan <jordan.clergue@gmail.com>
2022-09-08 11:31:28 +0200

    On retire la possibilté de créer un compte

--- src/Controller/ContratController.php
+++ src/Controller/ContratController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-#[Security('is_granted("ROLE_BAILLEUR")')]
+#[Security('is_granted("ROLE_ADMIN")')]
 
 class ContratController extends AbstractController
 {
